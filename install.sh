7a755280a817ae8c7c7815d4305bf51cb81ebf16
Fudong Bai <fudongbai@gmail.com>
2013-01-26 11:59:09 +0800

    bless: a hex editor
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

--- install.sh
+++ install.sh
@@ -29,6 +29,7 @@ sudo apt-get install rar unrar tree -y
 sudo apt-get install p7zip-full
 sudo apt-get install ascii
 sudo apt-get install dos2unix
+sudo apt-get install bless	# a hex editor
 
 # beyound compare
 sudo gdebi /opt/software/ubuntu/bcompare/bcompare-3.3.5.15075_i386.deb
