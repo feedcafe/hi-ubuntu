630c20e7cf06af44859f4235817c5c0344e0b9c9
Fudong Bai <fudongbai@gmail.com>
2013-05-11 16:06:00 +0800

    raring: bluez dependency
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

--- raring.sh
+++ raring.sh
@@ -91,6 +91,11 @@ sudo apt-get install bluez-hcidump
 # hcidump -w hcidump.cap
 # wireshark hcidump.cap
 sudo apt-get install wireshark -y
+
+# bluez-5.3
+sudo apt-get install libudev-dev
+sudo apt-get install libical-dev
+sudo apt-get install libreadline-dev -y
 ########################################################################
 
 
