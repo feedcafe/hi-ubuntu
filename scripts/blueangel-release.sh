bfb2f7f88ac1025d19bfffcabda8f56f2bbd38c5
Fudong Bai <baifudong@hisense.com>
2014-07-08 10:45:12 +0800

    update blueangel-release.sh
    Because of the package name and directory structure change, we need to
    do a little bit change to complete our job.
    
    Signed-off-by: Fudong Bai <baifudong@hisense.com>

--- scripts/blueangel-release.sh
+++ scripts/blueangel-release.sh
@@ -19,20 +19,18 @@ echo "copy blueangel binraires from ${SRC_DIR} to ${DEST_DIR}"
 echo "extracting so files"
 cd ${SRC_DIR}
 
-RAR_FILE=`find . -maxdepth 1 -name "*Hisense_Official_Patch_BlueAngel_V*.rar"`
-TMP_DATE=`echo $RAR_FILE | cut -d "_" -f1`
-TMP_VERSION=`echo $RAR_FILE | cut -d "_" -f6`
+TAR_FILE=`find . -maxdepth 1 -name "*Hisense_Official_Patch_V*.tar.gz"`
+TMP_DATE=`echo $TAR_FILE | cut -d "_" -f1`
+TMP_VERSION=`echo $TAR_FILE | cut -d "_" -f5`
 
 RELEASE_DATE=`echo $TMP_DATE | cut -d "/" -f2`
 RELEASE_VERSION=`echo $TMP_VERSION | cut -d "." -f1,2`
 
-echo $RAR_FILE
+echo $TAR_FILE
 echo $RELEASE_DATE
 echo $RELEASE_VERSION
 
-unrar x $RAR_FILE
-
-tar xvf ${RELEASE_DATE}_Hisense_Official_Patch_${RELEASE_VERSION}.tar.gz
+tar xvf ${TAR_FILE}
 
 # always sync to git repo
 cd ${DEST_DIR}/device/mstar/
@@ -45,15 +43,14 @@ cp *.so ${DEST_DIR}/device/mstar/common/libraries/bluetooth/mtk
 cp btconfig ${DEST_DIR}/device/mstar/common/preinstall/bluetooth
 cp mtkbt ${DEST_DIR}/device/mstar/common/preinstall/bluetooth
 
+# copy bluetooth driver#
+cp btmtk_usb.ko ${DEST_DIR}/device/mstar/mstarnapoli/preinstall/modules
+
 # commit to git repo
 cd ${DEST_DIR}/device/mstar/common
 rm ${DEST_DIR}/device/mstar/common/libraries/bluetooth/mtk/libbt-nano.so
 git commit -asm "add mtk prebuilt binaries (${RELEASE_VERSION}, ${RELEASE_DATE})"
 
-
-
-###################################copy bluetooth driver#######################
-cp ${SRC_DIR}/btmtk_usb.ko ${DEST_DIR}/device/mstar/mstarnapoli/preinstall/modules
 cd ${DEST_DIR}/device/mstar/mstarnapoli
 git commit -asm "bluetooth: add new mt7662 module driver"
 
