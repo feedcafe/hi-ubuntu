8b9c6517930bc499ddc316a1cc637392d34b3012
Fudong Bai <baifudong@hisense.com>
2015-04-07 09:02:31 +0800

    update ifconfig script
    Signed-off-by: Fudong Bai <baifudong@hisense.com>

--- android/script/ifconfig.sh
+++ android/script/ifconfig.sh
@@ -5,3 +5,10 @@ input keyevent 20
 input text 8.8.8.8
 input keyevent 20
 input text 192.168.7.1
+input keyevent 20
+
+# send keyevent KEYCODE_RIGHT
+input keyevent 22
+
+# send keyevent KEYCODE_ENTER
+input keyevent 66
