05ba9c26cba4bf52c08f070b3777d6654390b29c
Fudong Bai <fudongbai@gmail.com>
2016-05-11 20:20:05 +0800

    add script for Ubuntu 16.04 (Xenial Xerus)
    Signed-off-by: Fudong Bai <fudongbai@gmail.com>

---
+++ xenial.sh
@@ -0,0 +1,4 @@
+############################## misc ####################################
+# do not show hidden files
+gsettings set org.gtk.Settings.FileChooser show-hidden false
+############################## misc ####################################
